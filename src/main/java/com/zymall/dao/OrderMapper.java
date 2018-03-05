2b905553d931e632606e9a327f79b28ab3549e25
zhongyi <1454827785@qq.com>
2018-03-05 22:54:42 +0800

    'aaa'

---
+++ src/main/java/com/zymall/dao/OrderMapper.java
@@ -0,0 +1,17 @@
+package com.zymall.dao;
+
+import com.zymall.pojo.Order;
+
+public interface OrderMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Order record);
+
+    int insertSelective(Order record);
+
+    Order selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Order record);
+
+    int updateByPrimaryKey(Order record);
+}
